61a551131cca374a78279c4d6965e128731077f5
Alan Soares <alanrsoars@gmail.com>
2019-11-10 18:28:06 +1300

    Updates

---
+++ precache-manifest.025e7eab52a08a3ac9d1277bd121a388.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d281f077ea575537a55c8266431290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54b684da76b644a6be4",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a54b684da76b644a6be4",
+    "url": "/static/js/2.67dcf8b0.chunk.js"
+  },
+  {
+    "revision": "0b89af6ad54d19323fa7",
+    "url": "/static/js/main.78dd3cb0.chunk.js"
+  },
+  {
+    "revision": "4b853e159c7104cf4640",
+    "url": "/static/js/runtime-main.ecbe17c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
